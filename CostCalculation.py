04ce3f4daee3c0be201a1cedce5b8bc35fee09cd
radmanlo <radman.lotfiazar@gmail.com>
2022-04-29 15:45:29 +0300

    It is done. Working for making it more optimize.

---
+++ CostCalculation.py
@@ -0,0 +1,82 @@
+
+import urllib.request
+import json
+
+
+# this is a function for calculating the cost of items
+# the algorithm of this function is inspired by DFS
+# since it is going to the lowest node and start calculating from there
+def cost_calc(json_str):
+
+    # variables
+    count = 0
+    price = 0
+    cost = 0
+
+    # convert string object of json to json object
+    json_obj = json.loads(json_str)
+
+    for key in json_obj:
+        if key == "name":
+            pass
+        elif key == "count":
+            count = json_obj[key]
+        elif key == "price":
+            price = json_obj[key]
+            return count * price
+        elif key == "items":
+            jso_dump = json.dumps(json_obj[key])
+            # call the recursive
+            cost += cost_calc(jso_dump)
+            return count * cost
+        else:
+            jso_dump = json.dumps(key)
+            # call the recursive
+            cost += cost_calc(jso_dump)
+    return cost
+
+
+# the sample in the assignment is initialized
+data_set = {
+
+            "name": "a",
+            "count": 2,
+            "items": [
+                        {
+                            "name": "x",
+                            "count": 1,
+                            "price": 1
+                        },
+                        {
+                            "name": "y",
+                            "count": 2,
+                            "price": 2
+                        }
+            ]
+}
+
+# make a string object of json
+json_dump = json.dumps(data_set)
+# call cost_calc function for calculating cost
+cost1 = cost_calc(json_dump)
+
+# First Sample URL
+url = "https://prod-storyly-media.s3.eu-west-1.amazonaws.com/test-scenarios/sample_1.json"
+json_dump = urllib.request.urlopen(url)
+cost2 = cost_calc(json_dump.read())
+
+# Second Sample URL
+url = "https://prod-storyly-media.s3.eu-west-1.amazonaws.com/test-scenarios/sample_2.json"
+json_dump = urllib.request.urlopen(url)
+cost3 = cost_calc(json_dump.read())
+
+# Third Sample URL
+url = "https://prod-storyly-media.s3.eu-west-1.amazonaws.com/test-scenarios/sample_3.json"
+json_dump = urllib.request.urlopen(url)
+cost4 = cost_calc(json_dump.read())
+
+print("Cost1: ", cost1)
+print("Cost2: ", cost2)
+print("Cost3: ", cost3)
+print("Cost4: ", cost4)
+
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # AppSamurai_Assignment
 Cost Calculation Program
+
+# The Case:
+We want you to write a cost calculation program that finds the cost of the item that can contain
+several parts. For example, item 'a' consists of 1 of 'x' and 2 of 'y's. Each part has its own price,
+'x's price 1 unit and 'y's price 2 unit, so a 'a's cost is 1*1 + 2*2 = 5 units.
+Items' prices and parts are given in json format, you need to read json from url and decode. You
+will calculate the cost and write it to the console. Please note that item names are unique for
+each item.
+Json of given sample;
+{
+    "name": "a",
+    "count": 2,
+    "items": [
+        {
+            "name": "x",
+            "count": 1,
+            "price": 1
+        },
+        {
+            "name": "y",
+            "count": 2,
+            "price": 2
+        }
+    ]
+}
+Expected result;
+Cost of item 'a' is 10 units. // please note that 1 'a's cost is 5 but there are 2 of it so 2 * 5 = 10
+units.
+sample urls;
+https://prod-storyly-media.s3.eu-west-1.amazonaws.com/test-scenarios/sample_1.json
+https://prod-storyly-media.s3.eu-west-1.amazonaws.com/test-scenarios/sample_2.json
+https://prod-storyly-media.s3.eu-west-1.amazonaws.com/test-scenarios/sample_3.json
